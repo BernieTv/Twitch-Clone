20da461ba755d9824d6cdd4427a8844241753609
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2025-01-12 21:43:27 +0500

    update session utils file

--- backend/src/shared/utils/session.util.ts
+++ backend/src/shared/utils/session.util.ts
@@ -12,40 +12,44 @@ export function saveSession(
 	metadata: SessionMetadata
 ) {
 	return new Promise((resolve, reject) => {
-		req.session.createdAt = new Date()
-		req.session.userId = user.id
-		req.session.metadata = metadata
+		if (req.session) {
+			req.session.createdAt = new Date()
+			req.session.userId = user.id
+			req.session.metadata = metadata
 
-		req.session.save(err => {
-			if (err) {
-				return reject(
-					new InternalServerErrorException(
-						'Не удалось сохранить сессию'
+			req.session.save(err => {
+				if (err) {
+					return reject(
+						new InternalServerErrorException(
+							'Не удалось сохранить сессию'
+						)
 					)
-				)
-			}
+				}
 
-			resolve({ user })
-		})
+				resolve({ user })
+			})
+		}
 	})
 }
 
 export function destroySession(req: Request, configService: ConfigService) {
 	return new Promise((resolve, reject) => {
-		req.session.destroy(err => {
-			if (err) {
-				return reject(
-					new InternalServerErrorException(
-						'Не удалось завершить сессию'
+		if (req.session) {
+			req.session.destroy(err => {
+				if (err) {
+					return reject(
+						new InternalServerErrorException(
+							'Не удалось завершить сессию'
+						)
 					)
+				}
+
+				req.res.clearCookie(
+					configService.getOrThrow<string>('SESSION_NAME')
 				)
-			}
 
-			req.res.clearCookie(
-				configService.getOrThrow<string>('SESSION_NAME')
-			)
-
-			resolve(true)
-		})
+				resolve(true)
+			})
+		}
 	})
 }
