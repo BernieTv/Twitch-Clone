200a44fedf3bbb0c657e94908ea06b6a82936794
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2025-01-13 13:12:02 +0500

    update csv value #3

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://twitch-clone-ag5v.onrender.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://twitch-clone-ag5v.onrender.com/*;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
