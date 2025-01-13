321186c06e1bc81fd7920d52e62898934399ad35
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2025-01-13 13:03:59 +0500

    edit csp value

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,26 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
+const cspHeader = `
+    script-src 'self' https://twitch-clone-ag5v.onrender.com;
+`
+
 const withNextIntl = createNextIntlPlugin('./src/libs/i18n/request.ts')
 
 const nextConfig: NextConfig = {
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Content-Security-Policy',
+						value: cspHeader.replace(/\n/g, '')
+					}
+				]
+			}
+		]
+	},
 	reactStrictMode: true,
 	images: {
 		remotePatterns: [
