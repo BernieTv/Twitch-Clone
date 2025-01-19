a3cdc72b53b53d521d62dbcfe34281a038aa5fa5
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2025-01-20 01:14:33 +0500

    feat: fe update

--- frontend/src/libs/apollo-client.ts
+++ frontend/src/libs/apollo-client.ts
@@ -1,41 +1,47 @@
-import { ApolloClient, InMemoryCache, split } from '@apollo/client'
+import { split } from '@apollo/client'
+import { HttpLink } from '@apollo/client'
 import { WebSocketLink } from '@apollo/client/link/ws'
 import { getMainDefinition } from '@apollo/client/utilities'
-import createUploadLink from 'apollo-upload-client/createUploadLink.mjs'
+import {
+	ApolloClient,
+	InMemoryCache
+} from '@apollo/experimental-nextjs-app-support'
 
 import { SERVER_URL, WEBSOCKET_URL } from './constants/url.constants'
 
-const httpLink = createUploadLink({
-	uri: SERVER_URL,
-	credentials: 'include',
-	headers: {
-		'apollo-require-preflight': 'true'
-	},
-	fetchOptions: { credentials: 'include' }
-})
+export function makeClient() {
+	const httpLink = new HttpLink({
+		uri: SERVER_URL,
+		credentials: 'include',
+		headers: {
+			'apollo-require-preflight': 'true'
+		},
+		fetchOptions: { credentials: 'include' }
+	})
 
-const wsLink = new WebSocketLink({
-	uri: WEBSOCKET_URL,
-	options: {
-		reconnect: true
-	}
-})
+	const wsLink = new WebSocketLink({
+		uri: WEBSOCKET_URL,
+		options: {
+			reconnect: true
+		}
+	})
 
-const splitLink = split(
-	({ query }) => {
-		const definition = getMainDefinition(query)
+	const splitLink = split(
+		({ query }) => {
+			const definition = getMainDefinition(query)
 
-		return (
-			definition.kind === 'OperationDefinition' &&
-			definition.operation === 'subscription'
-		)
-	},
-	wsLink,
-	httpLink
-)
+			return (
+				definition.kind === 'OperationDefinition' &&
+				definition.operation === 'subscription'
+			)
+		},
+		wsLink,
+		httpLink
+	)
 
-export const client = new ApolloClient({
-	link: splitLink,
-	cache: new InMemoryCache(),
-	credentials: 'include'
-})
+	return new ApolloClient({
+		link: splitLink,
+		cache: new InMemoryCache(),
+		credentials: 'include'
+	})
+}
