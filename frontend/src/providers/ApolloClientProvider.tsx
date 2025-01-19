87c0736cdf7f02edbde6db9cc5dda51584c7aeaa
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2025-01-20 01:40:51 +0500

    vercel build error fix #3

--- frontend/src/providers/ApolloClientProvider.tsx
+++ frontend/src/providers/ApolloClientProvider.tsx
@@ -1,9 +1,54 @@
 'use client'
 
-import { ApolloNextAppProvider } from '@apollo/experimental-nextjs-app-support'
+import { split } from '@apollo/client'
+import { HttpLink } from '@apollo/client'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+import {
+	ApolloClient,
+	ApolloNextAppProvider,
+	InMemoryCache
+} from '@apollo/experimental-nextjs-app-support'
 import type { PropsWithChildren } from 'react'
 
-import { makeClient } from '@/libs/apollo-client'
+import { SERVER_URL, WEBSOCKET_URL } from '../libs/constants/url.constants'
+
+function makeClient() {
+	const httpLink = new HttpLink({
+		uri: SERVER_URL,
+		credentials: 'include',
+		headers: {
+			'apollo-require-preflight': 'true'
+		},
+		fetchOptions: { credentials: 'include' }
+	})
+
+	const wsLink = new WebSocketLink({
+		uri: WEBSOCKET_URL,
+		options: {
+			reconnect: true
+		}
+	})
+
+	const splitLink = split(
+		({ query }) => {
+			const definition = getMainDefinition(query)
+
+			return (
+				definition.kind === 'OperationDefinition' &&
+				definition.operation === 'subscription'
+			)
+		},
+		wsLink,
+		httpLink
+	)
+
+	return new ApolloClient({
+		link: splitLink,
+		cache: new InMemoryCache(),
+		credentials: 'include'
+	})
+}
 
 export function ApolloClientProvider({ children }: PropsWithChildren<unknown>) {
 	return (
